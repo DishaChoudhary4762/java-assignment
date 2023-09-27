0b5c94f4733bc54234ebc9e5b90d3d702a6da3a1
DishaChoudhary4762 <144053236+DishaChoudhary4762@users.noreply.github.com>
2023-09-27 12:57:02 +0530

    Create sort.java

---
+++ sort.java
@@ -0,0 +1,9 @@
+import java.utils.arrays;
+import java utilscollections;
+public class ex{
+    public static void main (String args[])
+    {
+        int a[]={10.30,2,5,6,7};
+    Array sort(a);
+    }
+}
