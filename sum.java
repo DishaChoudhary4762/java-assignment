f057039334b135357c120f2860f68eadbc016971
DishaChoudhary4762 <144053236+DishaChoudhary4762@users.noreply.github.com>
2023-09-27 12:55:15 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,13 @@
+
+public class Arr
+{
+	public static void main(String[] args) {
+	    int sum=0;
+	    for(int i:a)
+	    sum+=i;
+	    system.out.println("sum is :"+sum);
+	    doble avg=0;
+	    avg=sum/a.length;
+	    system.out.println(avg);
+	}
+}
