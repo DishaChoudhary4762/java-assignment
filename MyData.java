922511c253981ea8ef791eac9333edfe26b068cd
DishaChoudhary4762 <144053236+DishaChoudhary4762@users.noreply.github.com>
2023-11-08 12:54:14 +0530

    Add files via upload

---
+++ MyData.java
@@ -0,0 +1,28 @@
+//generic class example
+class MyData < E >
+{
+  E i;
+  public void set (E i1)
+  {
+    i = i1;
+  }
+  public E get ()
+  {
+    return (i);
+  }
+}
+
+public class Example
+{
+  public static void main (String[]args)
+  {
+    MyData < Integer > m1 = new MyData < Integer > ();
+    MyData < String > m2 = new MyData < String > ();
+    String s = "Hello";
+      m2.set (s);
+      System.out.println (m2.get ());
+    Integer i1 = 3;
+      m1.set (i1);
+      System.out.println (m1.get ());
+  }
+}
