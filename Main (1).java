ff8d3e7c4d3c6cdd6c7f8ebb4b8bd74cac389bd1
DishaChoudhary4762 <144053236+DishaChoudhary4762@users.noreply.github.com>
2023-11-08 12:32:17 +0530

    Add files via upload

--- Main (1).java
+++ Main (1).java
@@ -1,12 +1,25 @@
-class main {
-    public static void main(String[] args)
+public class Main
+{
+  public < E > void printArray (E[]s, E key)
+  {
+    for (int i = 0; i < s.length; i++)
+      {
+	if (key == s[i])
+	  {
+	    System.out.println ("the index of the element is:" + i);
+	  }
+      }
+  }
+  public static void main (String[]args)
+  {
+    Main m = new Main ();
+    String[]s1 =
     {
-        String first="JAVA";
-        String second="PYTHON";
-        String third="javascript";
-        String first="hello";
-        System.out.println(first);
-        System.out.println(second);
-        System.out.println(third);
-    }
+    "john", "om"};
+    m. < String > printArray (s1, "Om");
+    Integer[]i =
+    {
+    1, 2, 3, 4};
+    m. < Integer > printArray (i, 4);
+  }
 }
